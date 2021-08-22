687343f7c89db233ca5d7f6a7edf48cdf6da4fec
Lachlan Grant <grant.lachlan.j@gmail.com>
2021-08-22 23:41:28 +1000

    Added validation for env vars, cleaned up script, modified .gitignore

--- arbitrage.py
+++ arbitrage.py
@@ -11,18 +11,23 @@ load_dotenv()
 API_KEY = getenv('api_key')
 API_URL = getenv('api_url')
 API_ROUTE = getenv('api_route')
+API_CURRENCY = getenv('api_currency')
+
+if not API_KEY or not API_URL or not API_ROUTE:
+    exit('\nError: at least one of the environment variables is not set')
+
 if not API_URL.startswith('https://'):
     exit('\nError: api_url must begin with protocol (eg. https)')
 
+if not API_ROUTE.startswith('/'):
+    exit('\nError: api_route must begin with /')
 
-route = '/v1/cryptocurrency/listings/latest'
-
-url = API_URL + route
+url = API_URL + API_ROUTE
 
 parameters = {
     'start': '1',
     'limit': '10',
-    'convert': 'USD'
+    'convert': API_CURRENCY
 }
 
 headers = {
