7bd221e8264611712e8c1af6e195f8223793ba73
Lachlan Grant <grant.lachlan.j@gmail.com>
2022-01-31 20:23:53 +1100

    Simplified data fetched from API

--- arbitrage.py
+++ arbitrage.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-from requests import Request, Session
+from requests import Request, Session # Web HTTP request
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 from dotenv import load_dotenv
 from os import getenv
 from json import loads
+from json import dumps
 
 load_dotenv()
 
@@ -12,6 +13,7 @@ API_KEY = getenv('api_key')
 API_URL = getenv('api_url')
 API_ROUTE = getenv('api_route')
 API_CURRENCY = getenv('api_currency')
+
 
 if not API_KEY or not API_URL or not API_ROUTE:
     exit('\nError: at least one of the environment variables is not set')
@@ -25,8 +27,8 @@ if not API_ROUTE.startswith('/'):
 url = API_URL + API_ROUTE
 
 parameters = {
-    'start': '1',
-    'limit': '10',
+    'start': 1,
+    'limit': 10,
     'convert': API_CURRENCY
 }
 
@@ -38,10 +40,25 @@ headers = {
 session = Session()
 session.headers.update(headers)
 
+
+
 try:
+    # Send an API request to Coinmarketcap
     response = session.get(url, params=parameters)
-    data = loads(response.text)
-    print(data)
+    # Load the response into a JSON object
+    response = loads(response.text)
+    # Convert into a dictionary
+    dictionary = dumps(response, indent=4)
+    # Get the data only
+    data = response['data']
+    # Printing prettified data
+    #print(dumps(data, indent=4))
+
+    for dictionary in data:
+        dictionary.pop("tags")
+        print(dumps(dictionary, indent=4))
+
+
 except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
 
